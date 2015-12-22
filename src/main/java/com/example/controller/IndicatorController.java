4fe05f895d1ffaeabef140a98ac05cfe463c15ae
dem31 <dem31@me.com>
2015-12-22 10:26:45 +0100

    model

--- src/main/java/com/example/controller/IndicatorController.java
+++ src/main/java/com/example/controller/IndicatorController.java
@@ -19,13 +19,14 @@ import java.util.Calendar;
 public class IndicatorController {
 
     @RequestMapping
-    public String provideLink(String link) {
+    public String provideLink(HttpServletRequest request, Model m) {
     	link="http://real-chart.finance.yahoo.com/table.csv?g=d&ignore=.csv&";
         String[] parts=new String("2012-01-15").split("-");
         String year = String.format("%02d", Calendar.getInstance().get(Calendar.YEAR));
         String month = String.format("%02d", Calendar.getInstance().get(Calendar.MONTH)-1);
         String day = String.format("%02d", Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
         link+="e="+day+"&d="+month+"&f="+year+"&b="+parts[2]+"&a="+parts[1]+"&c="+parts[0]+"&s=BNP.PA";
+        m.addAttribute("link", link));
 
         return "indicator";
     }
