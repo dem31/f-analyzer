35b8236a1fd5b450b338ee040838f9cd177dd30d
dem31 <dem31@me.com>
2015-12-22 10:36:08 +0100

    MV

--- src/main/java/com/example/controller/IndicatorController.java
+++ src/main/java/com/example/controller/IndicatorController.java
@@ -20,14 +20,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndicatorController {
 
     @RequestMapping
-    public String provideLink(Model m) {
+    public String provideLink(ModelAndView m) {
     	String link="http://real-chart.finance.yahoo.com/table.csv?g=d&ignore=.csv&";
         String[] parts=new String("2012-01-15").split("-");
         String year = String.format("%02d", Calendar.getInstance().get(Calendar.YEAR));
         String month = String.format("%02d", Calendar.getInstance().get(Calendar.MONTH)-1);
         String day = String.format("%02d", Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
         link+="e="+day+"&d="+month+"&f="+year+"&b="+parts[2]+"&a="+parts[1]+"&c="+parts[0]+"&s=BNP.PA";
-        m.addAttribute("link", link);
+        m.addObject("link", link);
 
         return "indicator";
     }
