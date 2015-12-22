c776a0390a5fd007b13c25d586123303ed4bd3ef
dem31 <dem31@me.com>
2015-12-22 15:15:26 +0100

    persist indicators

---
+++ src/main/java/com/example/model/Analysis.java
@@ -0,0 +1,148 @@
+package com.example.model;
+
+import com.opencsv.CSVReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+public class Analysis {
+
+    public ArrayList<String> getDates() {
+        return dates;
+    }
+
+    public ArrayList<Double> getPrice() {
+        return price;
+    }
+
+    public ArrayList<Double> getPriceBench() {
+        return priceBench;
+    }
+
+    public ArrayList<Indicator> getIndicators() {
+        return indicators;
+    }
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private ArrayList<String> dates = new ArrayList<String>();
+    private ArrayList<Double> price = new ArrayList<Double>();
+    private ArrayList<Double> priceBench = new ArrayList<Double>();
+    
+    @ElementCollection
+    private ArrayList<Indicator> indicators = new ArrayList<Indicator>();
+
+    public Double getTe() {
+        return te;
+    }
+
+    public Double getIr() {
+        return ir;
+    }
+
+    private Double te;
+    private Double ir;
+
+    public Analysis(String link, String bench){
+
+        URL stockURL = null;
+        try {
+            stockURL = new URL(bench);
+            BufferedReader in = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+            CSVReader reader = new CSVReader(in);
+            List<String[]> pathBench = reader.readAll();
+            pathBench.remove(0);
+
+            stockURL = new URL(link);
+            in = new BufferedReader(new InputStreamReader(stockURL.openStream()));
+            reader = new CSVReader(in);
+            List<String[]> path= reader.readAll();
+            path.remove(0);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal = Calendar.getInstance();
+
+            int size=pathBench.size();
+            double d0=Double.parseDouble(pathBench.get(size-1)[6]);
+            Date cursorDate = sdf.parse(pathBench.get(size-1)[0]);
+            cal.setTime(cursorDate);
+            for (int i=pathBench.size()-1; i>=0; i--){
+                if (pathBench.get(i)[0].equals(sdf.format(cursorDate))) {
+                    dates.add(pathBench.get(i)[0]);
+                    priceBench.add(Double.parseDouble(pathBench.get(i)[6]) * 100 / d0);
+                    cal.add(Calendar.DATE, 7);
+                    cursorDate = new Date(cal.get(Calendar.YEAR)-1900,
+                            cal.get(Calendar.MONTH),
+                            cal.get(Calendar.DAY_OF_MONTH));
+                } else if (pathBench.get(i)[0].compareTo(sdf.format(cursorDate))>0) {
+                    int j=0;
+                    while (pathBench.get(i-j)[0].compareTo(sdf.format(cursorDate))>0 && i-j>0 && j<3) j++;
+                    dates.add(pathBench.get(i-j)[0]);
+                    priceBench.add(Double.parseDouble(pathBench.get(i - j)[6]) * 100 / d0);
+                    cal.add(Calendar.DATE, 7);
+                    cursorDate = new Date(cal.get(Calendar.YEAR)-1900,
+                            cal.get(Calendar.MONTH),
+                            cal.get(Calendar.DAY_OF_MONTH));
+                }
+            }
+
+            int j=0;
+            d0=Double.parseDouble(path.get(path.size()-1)[6]);
+            for (int i=path.size()-1; i>=0; i--) {
+                if (path.get(i)[0].equals(dates.get(j))) {
+                    price.add(Double.parseDouble(path.get(i)[6]) * 100 / d0);
+                    if (j<dates.size()-1) j++;
+                    else break;
+                } else if (path.get(i)[0].compareTo(dates.get(j))>=0){
+                    price.add(Double.parseDouble(path.get(i)[6]) * 100 / d0);
+                    if (j<dates.size()-1) j++;
+                }
+            }
+
+            double avgPerf=0.0;
+            for (int i=price.size()-1; i>=0; i--)
+                avgPerf += Math.pow(priceBench.get(price.size()-1) / priceBench.get(i), 52 / (i+1)) - Math.pow(price.get(price.size()-1) / price.get(i), 52 / (i+1));
+            avgPerf /= price.size()-1;
+
+            te=0.0;
+            for (int i=price.size()-1; i>=0; i--)
+                te+=Math.pow(Math.pow(priceBench.get(price.size() - 1) / priceBench.get(i), 52 / (i+1)) - Math.pow(price.get(price.size() - 1) / price.get(i), 52 / (i+1)) - avgPerf, 2);
+            te /= price.size()-1;
+
+            ir=(Math.pow(priceBench.get(price.size()-1) / priceBench.get(0), 52 / price.size()) - Math.pow(price.get(price.size()-1) / price.get(0), 52 / price.size()))/te ;
+
+
+
+            indicators.add(new Indicator(dates, price, priceBench, 13));
+            indicators.add(new Indicator(dates, price, priceBench, 26));
+            indicators.add(new Indicator(dates, price, priceBench, 52));
+
+        } catch (ParseException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
---
+++ src/main/java/com/example/model/Indicator.java
@@ -0,0 +1,68 @@
+package com.example.model;
+
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Indicator {
+
+	@Id
+    @GeneratedValue
+    private Integer id;
+	
+	private double alpha;
+    private double beta;
+    private double perf;
+    private double vol;
+	
+    public double getBeta() {
+        return beta;
+    }
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public double getPerf() {
+        return perf;
+    }
+
+    public double getVol() {
+        return vol;
+    }
+
+    public Indicator(ArrayList<String> dates, ArrayList<Double> price, ArrayList<Double> priceBench, int period){
+        int last=price.size()-1;
+
+        perf=Math.pow(price.get(last)/price.get(last-period), 52/period)-1;
+
+        Double perfBench=Math.pow(priceBench.get(last)/priceBench.get(last-period), 52/period)-1;
+
+        Double sum=0.0, sumBench=0.0, summ=0.0;
+        for (int i=last; i>last-period; i--)
+            sum += price.get(i);
+        Double avg=sum/period;
+        for (int i=last; i>last-period; i--)
+            sum += priceBench.get(i);
+        Double avgBench=sum/period;
+
+        for (int i=last; i>last-period; i--)
+            summ+=Math.pow(price.get(i)-avg, 2);
+        vol=Math.sqrt(period * summ / (period-1));
+
+        Double sum1=0.0, sum2=0.0;
+        for (int i=last; i>last-period; i--) {
+            sum1 += (price.get(i) - avg) * (priceBench.get(i) - avgBench);
+            sum2 += Math.pow(priceBench.get(i) - avgBench, 2);
+        }
+        beta=sum1/sum2;
+
+        alpha=perf-beta*perfBench;
+
+    }
+
+
+
+}
